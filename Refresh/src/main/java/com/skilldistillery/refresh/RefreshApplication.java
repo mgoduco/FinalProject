2cb63de1ebc290f6a1fa2e5d9cf1a7ef7ac6495f
Max <maxgoduco@gmail.com>
2022-06-22 12:03:21 -0400

    Create Boot REST project, set JPA project dependencies

---
+++ Refresh/src/main/java/com/skilldistillery/refresh/RefreshApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.refresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefreshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefreshApplication.class, args);
+	}
+
+}
---
+++ Refresh/src/test/java/com/skilldistillery/refresh/RefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.refresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefreshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
