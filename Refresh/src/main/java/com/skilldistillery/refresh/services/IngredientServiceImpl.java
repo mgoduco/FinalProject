64bd1b79ddf6d2fb8c3306cbaf5f08551acd8687
Matt Elmore <84700909+dawabar@users.noreply.github.com>
2022-06-23 17:19:26 -0400

    commented out broken Ingredient methods for now

--- Refresh/src/main/java/com/skilldistillery/refresh/services/IngredientServiceImpl.java
+++ Refresh/src/main/java/com/skilldistillery/refresh/services/IngredientServiceImpl.java
@@ -46,25 +46,25 @@ public class IngredientServiceImpl implements IngredientService {
 
 	@Override
 	public Ingredient createIngredient(String name, Ingredient ingredient) {
-		// TODO Auto-generated method stub
-		Recipe recipe = recipeRepo.findByName(name);
-		if (user != null) {
-			todo.setUser(user);
-			return todoRepo.saveAndFlush(todo);
-		}
-		return null;
+//		// TODO Auto-generated method stub
+//		Recipe recipe = recipeRepo.findByName(name);
+//		if (user != null) {
+//			todo.setUser(user);
+//			return todoRepo.saveAndFlush(todo);
+//		}
+//		return null;
 		return null;
 	}
 
-	@Override
-	public Todo create(String username, Todo todo) {
-		User user = userRepo.findByUsername(username);
-		if (user != null) {
-			todo.setUser(user);
-			return todoRepo.saveAndFlush(todo);
-		}
-		return null;
-	}
+//	@Override
+//	public Todo create(String username, Todo todo) {
+//		User user = userRepo.findByUsername(username);
+//		if (user != null) {
+//			todo.setUser(user);
+//			return todoRepo.saveAndFlush(todo);
+//		}
+//		return null;
+//	}
 	
 	@Override
 	public Ingredient updateIngredient(String username, int ingredientId, Ingredient ingredient) {
@@ -72,37 +72,37 @@ public class IngredientServiceImpl implements IngredientService {
 		return null;
 	}
 
+//	@Override
+//	public Todo update(String username, int tid, Todo todo) {
+//		Todo existing = todoRepo.findByIdAndUser_Username(tid, username);
+//		if (existing != null) {
+//			existing.setCompleted(todo.isCompleted());
+//			existing.setCompleteDate(todo.getCompleteDate());
+//			existing.setDescription(todo.getDescription());
+//			existing.setDueDate(todo.getDueDate());
+//			existing.setTask(todo.getTask());
+//			todoRepo.saveAndFlush(existing);
+//			return existing;
+//		}
+//		return null;
+//	}
+//	
 	@Override
 	public boolean hideIngredient(String username, int ingredientId) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
-	@Override
-	public Todo update(String username, int tid, Todo todo) {
-		Todo existing = todoRepo.findByIdAndUser_Username(tid, username);
-		if (existing != null) {
-			existing.setCompleted(todo.isCompleted());
-			existing.setCompleteDate(todo.getCompleteDate());
-			existing.setDescription(todo.getDescription());
-			existing.setDueDate(todo.getDueDate());
-			existing.setTask(todo.getTask());
-			todoRepo.saveAndFlush(existing);
-			return existing;
-		}
-		return null;
-	}
-
-	@Override
-	public boolean destroy(String username, int tid) {
-		boolean deleted = false;
-		Todo toDelete = todoRepo.findByIdAndUser_Username(tid, username);
-		if (toDelete != null) {
-			todoRepo.delete(toDelete);
-			deleted = true;
-		}
-		return deleted;
-	}
+//	@Override
+//	public boolean destroy(String username, int tid) {
+//		boolean deleted = false;
+//		Todo toDelete = todoRepo.findByIdAndUser_Username(tid, username);
+//		if (toDelete != null) {
+//			todoRepo.delete(toDelete);
+//			deleted = true;
+//		}
+//		return deleted;
+//	}
 
 	
 
