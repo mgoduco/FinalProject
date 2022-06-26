34668676bc1f8f3f0749f664d5d84713e9cdb84d
Max <maxgoduco@gmail.com>
2022-06-26 18:34:03 -0400

    recipe component stub

--- ngRefresh/src/app/components/recipe/recipe.component.ts
+++ ngRefresh/src/app/components/recipe/recipe.component.ts
@@ -1,15 +1,76 @@
+import { Recipe } from 'src/app/models/recipe';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { RecipeService } from 'src/app/services/recipe.service';
+import { UserService } from 'src/app/services/user.service';
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-recipe',
   templateUrl: './recipe.component.html',
-  styleUrls: ['./recipe.component.css']
+  styleUrls: ['./recipe.component.css'],
 })
 export class RecipeComponent implements OnInit {
+  recipe: null | Recipe = null;
+  newRecipe: Recipe = new Recipe();
+  recipes: Recipe[] = [];
+  selected: null | Recipe = null;
+  isSelected: boolean = false;
 
-  constructor() { }
+  // Icons
+  faArrowLeft = faArrowLeft;
+  constructor(
+    private recipeServ: RecipeService,
+    private userServ: UserService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
+    let idStr = this.route.snapshot.paramMap.get('id');
+    if (!this.selected && idStr) {
+      let idNum = Number.parseInt(idStr);
+      if (!isNaN(idNum)) {
+        this.recipeServ.show(idNum).subscribe({
+          next: (theRecipe) => {
+            this.selected = theRecipe;
+          },
+          error: (fail) => {
+            this.router.navigateByUrl('/recipeNotFound');
+          },
+        });
+      } else {
+        this.router.navigateByUrl('/invalidRecipeId');
+      }
+    }
+    this.reload();
+  }
+  reload(): void {
+    this.recipeServ.index().subscribe({
+      next: (dbRecipes) => {
+        this.recipes = dbRecipes;
+        console.log(dbRecipes);
+      },
+      error: (fail: any) => {
+        console.error('RecipeComp.reload: error');
+        console.error(fail);
+      },
+    });
+  }
+  displayTable() {
+    this.selected = null;
   }
 
+  createRecipe(recipe: Recipe) {
+    this.recipeServ.create(recipe).subscribe({
+      next: (newRecipe) => {
+        this.newRecipe = new Recipe();
+        this.reload();
+      },
+      error: (fail) => {
+        console.error('RecipeComponent.addTodo: error creating recipe');
+        console.error(fail);
+      },
+    });
+  }
 }
--- ngRefresh/src/app/services/recipe.service.ts
+++ ngRefresh/src/app/services/recipe.service.ts
@@ -8,18 +8,16 @@ import { Recipe } from '../models/recipe';
 import { AuthService } from './auth.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class RecipeService {
   private url = environment.baseUrl + 'api/recipes';
-
-
 
   constructor(
     private http: HttpClient,
     private auth: AuthService,
     private user: UserService
-  ) { }
+  ) {}
 
   getHttpOptions() {
     let options = {
@@ -36,7 +34,8 @@ export class RecipeService {
       catchError((err: any) => {
         console.log(err);
         return throwError(
-          () => new Error('RecipeService.index(): error retrieving Recipe: ' + err)
+          () =>
+            new Error('RecipeService.index(): error retrieving Recipe: ' + err)
         );
       })
     );
@@ -44,69 +43,89 @@ export class RecipeService {
 
   //?????????????????????????????????
   recipesByUsername(user: User): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.url + "/u/" +  user.username, this.getHttpOptions()).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError(
-          () => new Error('RecipeService.index(): error retrieving Recipes: ' + err)
-        );
-      })
-    );
+    return this.http
+      .get<Recipe[]>(this.url + '/u/' + user.username, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'RecipeService.index(): error retrieving Recipes: ' + err
+              )
+          );
+        })
+      );
   }
 
   recipesByNameandIngredient(keyword: string): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.url + "/search/" + keyword, this.getHttpOptions()).pipe(
-      catchError((err: any) => {
-        console.log(err);
-        return throwError(
-          () => new Error('RecipeService.index(): error retrieving Recipes: ' + err)
-        );
-      })
-    );
+    return this.http
+      .get<Recipe[]>(this.url + '/search/' + keyword, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error(
+                'RecipeService.index(): error retrieving Recipes: ' + err
+              )
+          );
+        })
+      );
   }
 
   create(recipe: Recipe): Observable<Recipe> {
+    recipe.active = true;
     return this.http.post<Recipe>(this.url, recipe, this.getHttpOptions()).pipe(
       catchError((err: any) => {
         console.error(err);
         return throwError(
-          () => new Error('RecipeService.create(): error creating Recipe: ' + err)
+          () =>
+            new Error('RecipeService.create(): error creating Recipe: ' + err)
         );
       })
     );
   }
   update(recipe: Recipe): Observable<Recipe> {
-    return this.http.put<Recipe>(`${this.url}/${recipe.id}`, recipe, this.getHttpOptions()).pipe(
-      // return this.http.put<Todo>(`{this.url}/${todo.id}`, todo).pipe(
-      catchError((err: any) => {
-        console.error(err);
-        return throwError(
-          () => new Error('RecipeService.update(): error updating Recipe: ' + err)
-        );
-      })
-    );
+    return this.http
+      .put<Recipe>(`${this.url}/${recipe.id}`, recipe, this.getHttpOptions())
+      .pipe(
+        // return this.http.put<Todo>(`{this.url}/${todo.id}`, todo).pipe(
+        catchError((err: any) => {
+          console.error(err);
+          return throwError(
+            () =>
+              new Error('RecipeService.update(): error updating Recipe: ' + err)
+          );
+        })
+      );
   }
 
   delete(id: number): Observable<Recipe> {
-    return this.http.delete<Recipe>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
-      catchError((err: any) => {
-        console.error(err);
-        return throwError(
-          () => new Error('RecipeService.delete(): error deleting Recipe: ' + err)
-        );
-      })
-    );
+    return this.http
+      .delete<Recipe>(`${this.url}/${id}`, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.error(err);
+          return throwError(
+            () =>
+              new Error('RecipeService.delete(): error deleting Recipe: ' + err)
+          );
+        })
+      );
   }
 
   show(id: number): Observable<Recipe> {
-    return this.http.get<Recipe>(`${this.url}/${id}`, this.getHttpOptions()).pipe(
-      catchError((err: any) => {
-      console.log(err);
-      return throwError(
-        () => new Error('RecipeService.show(): error retrieving todo: ' + err)
+    return this.http
+      .get<Recipe>(`${this.url}/${id}`, this.getHttpOptions())
+      .pipe(
+        catchError((err: any) => {
+          console.log(err);
+          return throwError(
+            () =>
+              new Error('RecipeService.show(): error retrieving todo: ' + err)
+          );
+        })
       );
-      })
-    );
   }
-
 }
