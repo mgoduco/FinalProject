1a6fb34c80dd9e1a9a3b0b8f8bc858f13c4c6add
Chris Brite <chrismbrite@gmail.com>
2022-06-26 15:56:05 -0700

    Search by recipe or ingredient

--- ngRefresh/src/app/services/recipe.service.ts
+++ ngRefresh/src/app/services/recipe.service.ts
@@ -55,7 +55,7 @@ export class RecipeService {
   }
 
   recipesByNameandIngredient(keyword: string): Observable<Recipe[]> {
-    return this.http.get<Recipe[]>(this.url + "/search/" + keyword, this.getHttpOptions()).pipe(
+    return this.http.get<Recipe[]>(this.url + "/search/" + keyword).pipe(
       catchError((err: any) => {
         console.log(err);
         return throwError(
