71ba61cd117e78814bef46c339459b9281a08c92
Chris Brite <chrismbrite@gmail.com>
2022-06-25 21:26:21 -0700

    Search Bar and Image optimizations

--- ngRefresh/src/app/components/navigation/navigation.component.ts
+++ ngRefresh/src/app/components/navigation/navigation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, TemplateRef } from '@angular/core';
 import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { faArrowLeft, faBacon, faBowlFood, faBowlRice, faBreadSlice, faBurger, faCarrot, faDrumstickBite, faEgg, faFish, faPieChart, faPizzaSlice, faTachographDigital, faUtensils, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-navigation',
@@ -7,6 +9,36 @@ import { NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent implements OnInit {
+
+  // Icons
+  faArrowLeft = faArrowLeft;
+   faBurger = faBurger;
+   faCarrot = faCarrot;
+   faBacon = faBacon;
+   faFish = faFish;
+   faPizzaSlice = faPizzaSlice;
+   faDrumstickBite = faDrumstickBite;
+   faUtensils = faUtensils;
+   faEgg = faEgg;
+   faBowlFood = faBowlFood;
+   faBowlRice = faBowlRice;
+   faBreadSlice = faBreadSlice;
+
+
+  icons: IconDefinition [] =  [
+    faEgg,
+    faBurger,
+    faCarrot,
+    faBacon,
+    faFish,
+    faPizzaSlice,
+    faDrumstickBite,
+    faUtensils,
+    faBowlFood,
+    faBowlRice,
+    faBreadSlice];
+
+
 
   constructor(private offcanvasService: NgbOffcanvas) { }
 
@@ -17,6 +49,9 @@ export class NavigationComponent implements OnInit {
     this.offcanvasService.open(content, { position: 'end' });
   }
 
+  randomIcon(){
+    return this.icons[Math.floor(Math.random() * this.icons.length)];
 
+  }
+  }
 
-}
